b7442bd8460f4585e5bafd2bcede818d16ab6057
CristhianBlack <barona98@hotmail.com>
2024-08-12 17:57:52 -0500

    mi primer commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import Mensaje from './Mensaje.js';
+
+
+
+function Description(){
+  return <p>Esta es la app del curso fullstack bootcamp</p>
+}
 
 function App() {
+  const mensaje = 'Hola Mundo desde variable'
+  const a =2
+  const b = 3
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Mensaje color='red' message='Estamos trabajando'/>
+      <Mensaje color='green' message='En un curso'/>
+      <Mensaje color='yellow' message='De React'/>
+    
+      <Description/>
+      <strong>Estamos trabajando en ello</strong>
+      <div>
+        <p>El resultado es:</p>
+        {a+b}
+      </div>
+      {mensaje}
+      <br/>
+      {+new Date()}
     </div>
   );
 }
---
+++ src/Mensaje.js
@@ -0,0 +1,5 @@
+function Mensaje(params) {
+  return <h1 style={{ color: params.color }}>{params.message}</h1>;
+}
+
+export default Mensaje;
